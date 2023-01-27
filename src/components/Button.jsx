0983113763c136ae18d153eb6bd3830d45ab6f14
ly li  <lilili846244@gmail.com>
2023-01-28 00:59:45 +0800

    footer updated

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "../style";
+
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] ${styles}`}
+    >
+      {" "}
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
